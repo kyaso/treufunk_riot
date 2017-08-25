bab35e97b15f4eb752436c1eebe4bfdee2833a81
kyaso_x550_mint <kirthihan.y@gmail.com>
2017-08-25 17:09:18 +0200

    print hex value of pll frac

--- treufunk_getset.c
+++ treufunk_getset.c
@@ -269,7 +269,7 @@ void treufunk_set_rx_pll_frac(treufunk_t *dev, uint32_t pll_frac)
     treufunk_sub_reg_write(dev, SR_RX_CHAN_FRAC_H, BIT24_H_BYTE(pll_frac));
     treufunk_sub_reg_write(dev, SR_RX_CHAN_FRAC_M, BIT24_M_BYTE(pll_frac));
     treufunk_sub_reg_write(dev, SR_RX_CHAN_FRAC_L, BIT24_L_BYTE(pll_frac));
-    DEBUG("_set_rx_pll_frac(): Set RX PLL frac value to 0x%lu\n", pll_frac);
+    DEBUG("_set_rx_pll_frac(): Set RX PLL frac value to 0x%06x\n", pll_frac);
 
     return 0;
 }
@@ -279,7 +279,7 @@ void treufunk_set_tx_pll_frac(treufunk_t *dev, uint32_t pll_frac)
     treufunk_sub_reg_write(dev, SR_TX_CHAN_FRAC_H, BIT24_H_BYTE(pll_frac));
     treufunk_sub_reg_write(dev, SR_TX_CHAN_FRAC_M, BIT24_M_BYTE(pll_frac));
     treufunk_sub_reg_write(dev, SR_TX_CHAN_FRAC_L, BIT24_L_BYTE(pll_frac));
-    DEBUG("_set_tx_pll_frac(): Set TX PLL frac value to 0x%lu\n", pll_frac);
+    DEBUG("_set_tx_pll_frac(): Set TX PLL frac value to 0x%06x\n", pll_frac);
 
     return 0;
 }
